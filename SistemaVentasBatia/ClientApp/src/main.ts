e465efc86d3c1cd036b977b5d0750cb2c990d946
Edgar Corona Esquivel <edgarc@grupobatia.com.mx>
2024-07-16 09:50:24 -0600

    HASHEO OK

--- SistemaVentasBatia/ClientApp/src/main.ts
+++ SistemaVentasBatia/ClientApp/src/main.ts
@@ -18,3 +18,6 @@ if (environment.production) {
 
 platformBrowserDynamic(providers).bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+
+  //LINEA PARA COMPROBAR EL HASHEO OKOKOKOKOKOKOKOKOKKKKKKKKKKKKK
