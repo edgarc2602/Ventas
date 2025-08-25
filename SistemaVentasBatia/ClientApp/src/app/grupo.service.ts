10b0a850cc9b1f8fa6ede64a4ab2f0b92af63566
alanm@grupobatia.com.mx <alanm@grupobatia.com.mx>
2025-08-25 10:13:53 -0600

    gruposervices

---
+++ SistemaVentasBatia/ClientApp/src/app/grupo.service.ts
@@ -0,0 +1,12 @@
+import { EventEmitter, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GrupoService {
+    grupoCambiado: EventEmitter<number> = new EventEmitter<number>();
+
+    cambiaGrupo(idGrupo: number) {
+        this.grupoCambiado.emit(idGrupo);
+    }
+}
---
+++ SistemaVentasBatia/ClientApp/src/app/models/usuario.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UsuarioGrupo = void 0;
+var UsuarioGrupo = /** @class */ (function () {
+    function UsuarioGrupo() {
+    }
+    return UsuarioGrupo;
+}());
+exports.UsuarioGrupo = UsuarioGrupo;
+//# sourceMappingURL=usuario.js.map
